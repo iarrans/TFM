d68a18406fcae2f96db4845a715cdbc599bdcbe0
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2024-06-19 14:01:17 +0200

    Players join w Controllers

---
+++ Assets/Scripts/MultiplayerManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MultiplayerManager : MonoBehaviour
+{
+    public static MultiplayerManager Instance;
+
+    public List<Transform> spawnPositions;
+
+    private int positionIndex;
+
+    public List<GameObject> players;
+
+    private void Awake()
+    {
+        Instance = this;
+        positionIndex = 0;
+    }
+
+    public void PlayerAdded(GameObject playerGO)
+    {
+        PlayerControls playerControls = playerGO.GetComponent<PlayerControls>();
+
+        //Añadimos a la lista de jugadores por si hiciera falta hacer algun efecto sobre ellos
+        players.Add(playerGO);
+
+        //Se asigna position de inicio
+        playerGO.transform.position = spawnPositions[positionIndex].position;
+
+        //Se asigna index de jugador
+        playerControls.playerNumber = positionIndex;
+
+        //Se cambia el texto para indicar num del jugador
+        int planerNumber = positionIndex + 1;
+        playerGO.name = "Player " + planerNumber;
+        playerGO.transform.GetChild(1).GetComponent<TextMeshPro>().text = "Player " + planerNumber;
+
+        //Se configuran las layers correctamente para los colliders exclusivos de jugadores
+        int PlayerLayer = LayerMask.NameToLayer("Default");
+        switch (positionIndex)
+        {
+            case 0:
+                PlayerLayer = LayerMask.NameToLayer("PlayerLayer1");
+                break;
+            case 1:
+                PlayerLayer = LayerMask.NameToLayer("PlayerLayer2");
+                break;
+            case 2:
+                PlayerLayer = LayerMask.NameToLayer("PlayerLayer3");
+                break;
+            case 3:
+                PlayerLayer = LayerMask.NameToLayer("PlayerLayer4");
+                break;;
+            default:
+                print("Incorrect player index.");
+                break;
+        }
+
+        playerGO.layer = PlayerLayer;
+        playerGO.transform.GetChild(0).gameObject.layer = PlayerLayer;
+
+        positionIndex++;
+    }
+}
--- Assets/Scripts/NewVersion/PlayerControls.cs
+++ Assets/Scripts/NewVersion/PlayerControls.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,14 +16,33 @@ public class PlayerControls : MonoBehaviour
     public Transform body;
     public int playerNumber;
     public MirrorType mirrorType;
-    public LayerMask playerLayer;
-   
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        body = transform.GetChild(0);
+    }
+
+    public void PlayerJoined(PlayerInput input)
+    {
+        Debug.Log("Player joined!");
+        PlayerSettings(input.gameObject);
+    }
+
+    private void PlayerSettings(GameObject playerGO)
+    {
+        MultiplayerManager.Instance.PlayerAdded(playerGO);
+    }
 
     private void FixedUpdate()
     {
-            rb = transform.GetComponent<Rigidbody>();
-            body = transform.GetChild(0);
         Move();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move");
+        move = context.ReadValue<Vector2>();
     }
 
     void Move()
@@ -45,7 +66,16 @@ public class PlayerControls : MonoBehaviour
 
         rb.AddForce(velocityChange, ForceMode.VelocityChange);
     }
-    
+
+    public void OnRotate(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Rotate");
+            CharacterRotate();
+        }
+    }
+
     public void CharacterRotate()
     {
         float degreeNumber = 0;
@@ -66,11 +96,6 @@ public class PlayerControls : MonoBehaviour
         }
 
         body.Rotate(0, degreeNumber, 0.0f, Space.Self);
-    }
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
     }
 
 }
