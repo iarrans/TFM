4821d260a0a1a1f1ea7419e2f517a7e37c8f444e
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2024-06-19 11:11:50 +0200

    Time Death Removed

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -10,32 +10,25 @@ public class TimeManager : MonoBehaviour
     public TextMeshProUGUI textoTiempo; //texto cuenta atrás
 
     public float tiempoTranscurrido;
-    public float tiempoRestante; 
+
     public void Start()
     {
-        int minutos = Mathf.Max(Mathf.FloorToInt(tiempoRestante / 60), 0);     //Extraemos los minutos
-        int segundos = Mathf.Max(Mathf.FloorToInt(tiempoRestante % 60), 0);    //Extraemos los segundos
+        int minutos = Mathf.Max(Mathf.FloorToInt(tiempoTranscurrido / 60), 0);     //Extraemos los minutos
+        int segundos = Mathf.Max(Mathf.FloorToInt(tiempoTranscurrido % 60), 0);    //Extraemos los segundos
         if (segundos >= 10) textoTiempo.text = minutos + ":" + segundos;                    //Si los segundos son mayores o iguales que 10, concatenamos y construimos el texto
         else textoTiempo.text = minutos + ":" + "0" + segundos;
     }
 
     public void Update()
     {
-      
-        tiempoRestante -= Time.deltaTime; 
         tiempoTranscurrido += Time.deltaTime; 
 
         //parseo del tiempo a formato contador
-        int minutos = Mathf.Max(Mathf.FloorToInt(tiempoRestante / 60), 0);     //Extraemos los minutos
-        int segundos = Mathf.Max(Mathf.FloorToInt(tiempoRestante % 60), 0);    //Extraemos los segundos
+        int minutos = Mathf.Max(Mathf.FloorToInt(tiempoTranscurrido / 60), 0);     //Extraemos los minutos
+        int segundos = Mathf.Max(Mathf.FloorToInt(tiempoTranscurrido % 60), 0);    //Extraemos los segundos
         if (segundos >= 10) textoTiempo.text = minutos + ":" + segundos;                    //Si los segundos son mayores o iguales que 10, concatenamos y construimos el texto
         else textoTiempo.text = minutos + ":" + "0" + segundos;                             //Si los segundos son menors que 10, añadimos un 0 a nuestros segundos, concatenamos
 
-        if (tiempoRestante <= 0 ) 
-        {
-            textoTiempo.text = "Fin";
-            GameManager.Instance.LevelFailed();
-        }
     }
 
 }
