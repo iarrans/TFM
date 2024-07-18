fdb1a812d21b7bf37ac8fa53de62f5b4f0e7da0e
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2024-07-18 20:12:50 +0200

    Level Ending CameraMove

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,11 +18,14 @@ public class GameManager : MonoBehaviour
 
     public bool playingLevel = true;
 
+    public Transform winningCamPosition;
+
 
     private void Awake()
     {
         Instance = this;
         scapedChars = 0;
+        DOTween.Init();
     }
 
     public void CheckScaped()
@@ -30,6 +34,8 @@ public class GameManager : MonoBehaviour
         if (scapedChars == requiredPlayers)
         {
             UIManager.Instance.ShowVictoryScreen();
+            Camera.main.transform.DOMove(winningCamPosition.position,3);
+            Camera.main.transform.DORotate(winningCamPosition.rotation.eulerAngles, 3);
         }
     }
 }
