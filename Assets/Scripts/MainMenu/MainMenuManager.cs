5b190e5ef97667fb5f9e1e81a9f922866383094d
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2024-07-18 16:36:23 +0200

    main menu fix

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
